f858ca1ba384f7cdf1ea54bfd33356d2dcf840fa
Leestan360 <stanleymayore2@gmail.com>
2022-03-14 10:58:38 +0000

    C Language

---
+++ 5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * print numbers from 0 to 10
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
