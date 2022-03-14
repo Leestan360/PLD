09726fb41d771388327ae844bff5ec1c8f71bdb6
Leestan360 <stanleymayore2@gmail.com>
2022-03-14 08:41:10 +0000

    C language

---
+++ 5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * print a phrase followed with a new line
+ * use printf
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
